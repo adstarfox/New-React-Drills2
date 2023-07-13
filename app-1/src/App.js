353e211e9e8ef008835f720fed10d3e6cf17db4e
Alex Durrant <alexjdurrant@gmail.com>
2023-07-13 13:50:52 -0600

    finished app-1

--- app-1/src/App.js
+++ app-1/src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
+import { useState } from 'react'
 import './App.css';
 
 function App() {
+  const [letters, setLetters] = useState('')
+
+  console.log(letters)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" onChange={e => setLetters(e.target.value)} />
+      <p>{letters}</p>
     </div>
   );
 }
