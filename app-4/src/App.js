ee4b5ff3e8b2d4c078ac0cbe8350d927aa175957
Alex Durrant <alexjdurrant@gmail.com>
2023-09-28 16:52:25 -0600

    app 4 complete

--- app-4/src/App.js
+++ app-4/src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
 
 function App() {
+  const [user, setUser] = useState('');
+  const [pass, setPass] = useState('');
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    alert(`Username: ${user} Password: ${pass}`)
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={onSubmit}>
+        <input type="text" onChange={e => setUser(e.target.value)}/>
+        <input type="text" onChange={e => setPass(e.target.value)}/>
+        <button>Login</button>
+      </form>
     </div>
   );
 }
