fe4c8f9ad95c34730a73ccf162c99cc3b746f728
Alex Durrant <alexjdurrant@gmail.com>
2023-10-02 22:25:26 -0600

    app 5 complete

--- app-5/src/App.js
+++ app-5/src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import {useState} from 'react'
+import Image from './Image'
 import './App.css';
 
 function App() {
+  const [imgURL, setImgURL] = useState(`https://i.kym-cdn.com/entries/icons/original/000/029/461/Phineas_and_Ferb_Dude__We're_Getting_the_Band_Back_Together_0-6_screenshot.png`)
+  const [newUrl, setNewUrl] = useState('')
+
+  const submit = (e) => {
+    e.preventDefault()
+    // console.log(newUrl)
+    setImgURL(newUrl)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='images'>
+      <form onSubmit={submit}>
+        <input type="text" placeholder='Please enter an image url' onChange={(e) => setNewUrl(e.target.value)}/>
+        <button>Submit</button>
+      </form>
+      <Image imgURL={imgURL}/>
     </div>
   );
 }
---
+++ app-5/src/Image.jsx
@@ -0,0 +1,8 @@
+const Image = ({imgURL}) => {
+    // console.log(imgURL)
+    return(
+        <img src={imgURL} alt="No Pic Avaiable" />
+    )
+}
+
+export default Image
